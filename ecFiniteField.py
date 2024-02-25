0c6a8b59798e2074da849a34a0d93dc5718776f4
Math Akemi <objective.axioma@gmail.com>
2024-02-25 13:55:55 -0500

    Implementation is partially functional now without plotting. Next to add is algorithm to find order of the curve over any Fq and furthermore the order of a point.

--- ecFiniteField.py
+++ ecFiniteField.py
@@ -29,8 +29,8 @@ class Point():
             self.curve = E
         else:
             return "A curve must be provided with P."
-        self.x = x
-        self.y = y
+        self.x = x % E.char
+        self.y = y % E.char
 
     def __neg__(self):
         if self.x == float("inf") or self.y == float("inf"):
@@ -67,7 +67,8 @@ class Point():
 # EllipticCurve.q: order of the finite field Fq over which the curve is defined          #
 # EllipticCurve.a: first coefficient of Weierstrass or general Weierstrass if char = 2   #
 # EllipticCurve.b: second coefficient of Weierstrass or  general Weierstrass is char = 2 #
-# EllipticCurve.c: the third coefficient of gen. Weierst., only when char = 2            #
+# EllipticCurve.c: third coefficient of gen. Weierst., only when char = 3 or 2 and t = 1 #
+# EllipticCurve.t: whether or not a1 = 0 in gen. Weierst., only when char = 2            #
 # EllipticCurve.dis: discriminant of the curve, must be != 0                             #
 #                                                                                        #
 # isOnCurve(P): returns 1 if P is on curve, 0 otherwise                                  #
@@ -81,16 +82,69 @@ class Point():
 class EllipticCurve():
 
     # Initializes an EllipticCurve object with characteristic and coefficients
-    def __init__(self, char, q, a, b, c=None):
+    def __init__(self, char, q, a, b, c=None, t=None):
         self.char = char
         self.q = q
-        self.a = a
-        self.b = b
+        self.a = a % char
+        self.b = b % char
+        if isinstance(t, int):
+            if t == 1 or t == 0:
+                self.t = t
         if self.char == 2 and (isinstance(c, int) or isinstance(c, float)):
-            self.c = c
+            self.eqn = "n"          # n for non-zero, i.e. a1 != 0
+            if self.t == 0:
+                self.c = c % 2 
+                self.eqn = "z"      # z for zero, i.e. a1 = 0
 
-        self.eqn = "w"
+        if self.char != 2:
+            self.eqn = "w"
+
+        if self.char == 3:
+            self.eqn = "t"      # t for three, i.e. char = 3
+            self.c = c % 3
+
+    def isOnCurve(self, P):
+        if self.char != 2 and self.char != 3:
+            toCheck = (P.y)**2 - (P.x)**3 - self.a*(P.x) - self.b
+            if toCheck == 0:
+                return 1
+            else:
+                return 0
+        elif self.char == 3:
+            toCheck = (P.y)**2 - (P.x)**3 - self.a*(P.x)**2 - self.b*(P.x) - self.c
+
+    def orderOfBaseField(self):
+        return 0
+
+    def orderOfCurve(self):
+        return 0
+
+    def orderOfPoint(self, P):
+        return 0
+
+    def __str__(self):
+        # When characteristic is 2
         if self.char == 2:
-            # need to add cases for a0 = 0 and a0 != 0
+            # a1 != 0
+            if self.eqn == 'n':
+                return str("y^2 + xy = x^3 + " + str(self.a) + "x^2 + " + str(self.b))
+            
+            # a1 = 0
+            if self.eqn == 'z':
+                return str("y^2 + " + str(self.a) + "y = x^3 + " + str(self.b) + "x + " + str(self.c))
 
+        # When characteristic is 3
+        if self.char == 3:
+            return str("y^2 = x^3 + " + str(self.a) + "x^2 + " + str(self.b) + "x + " + str(self.a))
 
+        # When characteristic is not 2 or 3
+        if self.a > 0 and self.b > 0:
+            return str("y^2 = x^3 + " + str(self.a) + "x + " + str(self.b))
+        elif self.a > 0 and self.b < 0:
+            return str("y^2 = x^3 + " + str(self.a) + "x - " + str(-self.b))
+        elif self.a < 0 and self.b > 0:
+            return str("y^2 = x^3 - " + str(-self.a) + "x + " + str(self.b))
+        elif self.a < 0 and self.b < 0:
+            return str("y^2 = x^3 - " + str(-self.a) + "x - " + str(-self.b))
+
+    __repr__ = __str__
